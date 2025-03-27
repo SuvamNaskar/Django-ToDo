ce1e315f48e0d9c1d6fb780d679244aa51c50164
Suvam Naskar <suvamnaskar780@gmail.com>
2025-03-27 21:31:00 +0530

    NEW: Task insert and Task view

--- todo/home/views.py
+++ todo/home/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Tasks
 
 def home(req):
+    
+    if req.method == 'POST':
+        data = req.POST
+        task = data.get('newTask')
+
+        Tasks.objects.create(
+            task = task,
+            status = 1
+        )
+        
+    res = Tasks.objects.all()
     context = {
                 'page': 'Django ToDo',
+                'tasks': res,
               }
     return render(req, 'home.html', context)
