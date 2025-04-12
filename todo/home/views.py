1cf934576754acaa1d2298bd640c1c6a944a90c2
Suvam Naskar <suvamnaskar780@gmail.com>
2025-04-12 14:58:00 +0530

    NEW: Login Required

--- todo/home/views.py
+++ todo/home/views.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.contrib import messages
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='login')
 def home(req):
     
     if req.method == 'POST':
@@ -24,11 +26,13 @@ def home(req):
               }
     return render(req, 'home.html', context)
 
+@login_required(login_url='login')
 def delete_task(req, task_id):
     task = get_object_or_404(Tasks, id=task_id)
     task.delete()
     return redirect('home')
 
+@login_required(login_url='login')
 def toggle_status(req, task_id):
     task = get_object_or_404(Tasks, id=task_id)
     task.status = 0 if task.status == 1 else 1
