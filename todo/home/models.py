8b02755a13bf9e239f8b20fbb03e84e261a2203d
Suvam Naskar <suvamnaskar780@gmail.com>
2025-04-12 09:17:29 +0530

    ADD: User model

--- todo/home/models.py
+++ todo/home/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Tasks(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     task = models.CharField(max_length=255)
     status = models.IntegerField(default=0)
