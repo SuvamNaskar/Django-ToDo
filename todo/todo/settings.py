f4947bccf89c08d78efbf05d85c5edf2f39401dc
Suvam Naskar <suvamnaskar780@gmail.com>
2025-04-23 05:58:20 +0530

    NEW: Domain Allowed as Host

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY =os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['13.200.203.157', '127.0.0.1']
+ALLOWED_HOSTS = ['13.200.203.157', '127.0.0.1', 'todo.django.suvam.tech']
 
 
 # Application definition
